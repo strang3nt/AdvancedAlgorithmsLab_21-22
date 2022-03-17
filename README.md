b2c32e619e9e93518b9d76d1c349b1492d23cffd
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-03-17 16:12:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AdvancedAlgorithmsLab_21-22
+
+This repository contains the code produced for Unipd Advanced Algorithms course assignments.
---
+++ lab1/Main.fs
@@ -0,0 +1,15 @@
+﻿
+
+// For more information see https://aka.ms/fsharp-console-apps
+open Plotly.NET
+open Plotly.NET.ImageExport
+
+let xData = [0. .. 10.]
+let yData = [0. .. 10.]
+
+Chart.Point(xData,yData)
+|> Chart.savePNG(
+    "./out/example1",
+    Width=300,
+    Height=300
+)
