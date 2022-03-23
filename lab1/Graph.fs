9d272de6b5ad517f9e4522e87e8a75c4d7fbef32
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-03-23 10:07:30 +0100

    Graph with arrays and Adjacency List

--- lab1/Graph.fs
+++ lab1/Graph.fs
@@ -1,16 +1,36 @@
+module lab1.Graph
 
 open FSharp.Collections
 
 type Node = int
+type Nodes = Node array
 type Weight = int
-type something = Node * Node array
-type Edge = Node * Node * Weight
-type Graph = something array * Edge array
 
-type Graph1 = something array * Edge array
+type Edge = int * Weight // int is index of node in the Nodes type
+type Edges = Edge array
 
-type Neighbour = Node * Weight
-type something2 = Node * Neighbour array
-type Graph2 = something array
+type AdjList = Edges array
 
-type Graph3 = Weight Weight array2D
+type Graph = Nodes * AdjList
+
+let w ((_, l) : Graph) (n1: int) (n2: int) : Weight =
+    let (_, w) = Array.find (fun (x, _) -> x = n2) l[n1]
+    w
+    
+let areAdjacent ((_, l): Graph) (n1 : int) (n2 : int) : bool =
+    match Array.tryFind (fun (x, _) -> x = n2) l[n1] with 
+    | Some (_) -> true
+    | None -> false
+
+let addNode ((ns, l) : Graph) (n : Node) : Graph =
+    (Array.append ns [| n |], l)
+
+let addEdge ((ns, l) : Graph) (n1 : Node) (n2 : Node) (w : Weight): Graph =
+    let idx1 = Array.findIndex (fun x -> x = n1) ns
+    let idx2 = Array.findIndex (fun x -> x = n2) ns
+    let edge1 = idx1, w
+    let edge2 = idx2, w
+    (
+        ns, 
+        (Array.updateAt idx1 (Array.append l[idx1] [| edge1 |]) l) |> (Array.updateAt idx2 (Array.append l[idx2] [| edge2 |]))
+    )
--- lab1/Parsing.fs
+++ lab1/Parsing.fs
@@ -1,5 +1,7 @@
 module lab1.Parsing
 
+
+open lab1.Graph
 open System
 open System.IO
 
