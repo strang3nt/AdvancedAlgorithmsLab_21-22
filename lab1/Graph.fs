d04b6fbd5888d7dd62c26fee9606e3c1097a8f3b
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-03-23 16:28:48 +0100

    Added index of Node in Adjacency List type

--- lab1/Graph.fs
+++ lab1/Graph.fs
@@ -1,4 +1,4 @@
-module lab1.Graph
+module lab1.Graphs
 
 open FSharp.Collections
 
@@ -9,28 +9,37 @@ type Weight = int
 type Edge = int * Weight // int is index of node in the Nodes type
 type Edges = Edge array
 
-type AdjList = Edges array
+type AdjList = (int * Edges) array
 
 type Graph = Nodes * AdjList
 
 let w ((_, l) : Graph) (n1: int) (n2: int) : Weight =
-    let (_, w) = Array.find (fun (x, _) -> x = n2) l[n1]
+    let (_, edges) = l[n1]
+    let (_, w) = Array.find (fun (x, _) -> x = n2) edges
     w
     
 let areAdjacent ((_, l): Graph) (n1 : int) (n2 : int) : bool =
-    match Array.tryFind (fun (x, _) -> x = n2) l[n1] with 
+    let (_, edges) = l[n1]
+    match Array.tryFind (fun (x, _) -> x = n2) edges with 
     | Some (_) -> true
     | None -> false
 
 let addNode ((ns, l) : Graph) (n : Node) : Graph =
-    (Array.append ns [| n |], l)
+    (Array.append ns [| n |], Array.append l [| (ns.Length, Array.empty) |] )
 
 let addEdge ((ns, l) : Graph) (n1 : Node) (n2 : Node) (w : Weight): Graph =
     let idx1 = Array.findIndex (fun x -> x = n1) ns
     let idx2 = Array.findIndex (fun x -> x = n2) ns
     let edge1 = idx1, w
-    let edge2 = idx2, w
+    let edge2 = idx2, w 
+    let (_, oldAdjEdge1) = l[idx1]
+    let (_, oldAdjEdge2) = l[idx2]
     (
         ns, 
-        (Array.updateAt idx1 (Array.append l[idx1] [| edge1 |]) l) |> (Array.updateAt idx2 (Array.append l[idx2] [| edge2 |]))
+        (Array.updateAt idx1 (idx1, (Array.append oldAdjEdge1 [| edge1 |])) l) |> (Array.updateAt idx2 (idx2, (Array.append oldAdjEdge2 [| edge2 |])))
     )
+
+let nodeExists ((ns, _) : Graph) (n : Node) : bool =
+    match Array.tryFind(fun x -> x = n) ns with
+    | Some (_) -> true
+    | _ -> false
--- lab1/Parsing.fs
+++ lab1/Parsing.fs
@@ -1,10 +1,24 @@
 module lab1.Parsing
 
-
-open lab1.Graph
+open lab1.Graphs
 open System
 open System.IO
 
 let file filename =
     let path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + "/graphs/" + filename
     File.ReadAllLines(path)
+
+let buildGraph (filename : String) : Graph =
+    let str = file filename
+    let mutable g : Graph = Array.empty, Array.empty
+    for s in str do
+        let edgeArr = Array.map (fun x -> int x ) (s.Split [| ' '|])
+
+        if not (nodeExists g edgeArr[0]) then
+            g <- addNode (g) (edgeArr[0])
+
+        if not (nodeExists g edgeArr[1]) then
+            g <- addNode (g) (edgeArr[1])
+
+        g <- addEdge g edgeArr[1] edgeArr[2] edgeArr[3]
+    g
