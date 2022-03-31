d08e71d73841917661370cb0ae232792e0c4e909
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-03-31 20:27:32 +0200

    Print expected asymptotic growth graph and data

--- lab1/Main.fs
+++ lab1/Main.fs
@@ -8,6 +8,36 @@ open Plotly.NET
 open Plotly.NET.ImageExport
 open System.IO
 
+let printData (graphsSize : int array) (runtimes : float array) = 
+    let ratios = [ for i=0 to graphsSize.Length - 2 do yield System.Math.Round (runtimes[i+1]/runtimes[i]) ]
+    let c_estimates = [ for i = 0 to graphsSize.Length - 1 do yield System.Math.Round (runtimes[i]/float graphsSize[i], 3) ]
+    printfn "Size\tTime(ns)\tCostant\t\tRatio"
+    printfn "%s" (String.replicate 50 "-")
+    for i = 0 to graphsSize.Length - 1 do
+        printfn $"{graphsSize[i]}\t{runtimes[i]}\t{c_estimates[i]}\t{ratios[i]}"
+    printfn "%s" (String.replicate 50 "-")
+    c_estimates
+
+let printGraph (graphsSize : int array) (runtimes : float array) (reference : int list) = 
+    [
+        Chart.Line(graphsSize, runtimes)
+        |> Chart.withTraceInfo(Name="Measured time")
+        |> Chart.withLineStyle(Width=2.0, Dash=StyleParam.DrawingStyle.Solid)
+
+        Chart.Line(graphsSize, reference)
+        |> Chart.withTraceInfo(Name="Expected asymptotic growth")
+        |> Chart.withLineStyle(Width=2.0, Dash=StyleParam.DrawingStyle.Solid) 
+    ]
+    |> Chart.combine
+    |> Chart.withXAxisStyle("Graph size")
+    |> Chart.withYAxisStyle("Run times")
+    |> Chart.withTitle("Simple Kruskal")
+    |> Chart.savePNG(
+        "./out/simpleKruskal",
+        Width=500,
+        Height=500
+    )
+
 let measureRunTime f input numCalls =
     let watch = System.Diagnostics.Stopwatch()
     watch.Start()
@@ -35,17 +65,20 @@ let main argv =
     printfn "%i graphs built" graphs.Length
 
     // simple kruskal runtimes
-    let skRunTimes = Array.Parallel.map (fun g -> measureRunTime (simpleKruskal) g 10000) graphs
+    let skRunTimes = 
+        Array.Parallel.map (fun g -> measureRunTime (simpleKruskal) g 10000) graphs
+        |> Array.chunkBySize 4
+        |> getRunTimeBySize
 
-    Chart.Line(graphsSize, skRunTimes |> Array.chunkBySize 4 |> getRunTimeBySize)
-    |> Chart.withXAxisStyle("Graph size")
-    |> Chart.withYAxisStyle("Run times")
-    |> Chart.withTitle("Simple Kruskal")
-    |> Chart.savePNG(
-        "./out/simpleKruskal",
-        Width=500,
-        Height=500
-    )
+    let constant = 
+        printData graphsSize skRunTimes 
+        |> List.last
+        |> round
+        |> int
+
+    let reference = [ for i in graphsSize do yield i * constant ]
+
+    printGraph graphsSize skRunTimes reference
 
     printfn "Finished simple Kruskal"
 
