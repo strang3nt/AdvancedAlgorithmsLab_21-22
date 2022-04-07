f84110f359fe81e199de6dfaf2f2580dc9853efd
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-04-08 01:24:16 +0200

    Added edges sort function

--- lab1/Graph.fs
+++ lab1/Graph.fs
@@ -22,8 +22,8 @@ let w n1 n2 (Graph (_, es, adj) as g) : Weight Option =
         Some w
     else None
 
-let totalWeight ( Graph (_, e, _) ) : int =
-    Array.sumBy (fun (_, _, w) -> w) e
+let totalWeight ( Graph (_, es, _) ) : int =
+    Array.sumBy (fun (_, _, w) -> w) es
 
 let private searchNode n (Graph (ns, _, _)) = 
     Array.findIndex (fun x -> n = x) ns
@@ -31,14 +31,19 @@ let private searchNode n (Graph (ns, _, _)) =
 let private setEdge n1 n2 w i (Graph (ns, es, adj) as g) : unit =
     let idx1 = searchNode n1 g
     let idx2 = searchNode n2 g
-    Array.set adj idx1 (es.Length :: adj[idx1]) 
-    Array.set adj idx1 (es.Length :: adj[idx2])
-    Array.set es i (idx1, idx2, w)
+    adj[idx1] <- (es.Length :: adj[idx1]) 
+    adj[idx2] <- (es.Length :: adj[idx2])
+    es[i] <- (idx1, idx2, w)
 
 let buildGraph (edges: int array array) (sizes : int array) : Graph =
     let nodes = Array.create (sizes[1]*2) -1
-    Array.Parallel.iteri (fun i (e: int array) -> Array.set nodes (i*2) e[0]; Array.set nodes (i*2+1) e[1]) edges
+    Array.Parallel.iteri (fun i (e: int array) -> nodes[i*2] <- e[0]; nodes[i*2+1] <- e[1]) edges
     let nodes = nodes |> Array.distinct
     let g = Graph ( nodes, (Array.create sizes[1] (0,0,0)), (Array.create nodes.Length List.empty) )
-    Array.Parallel.iteri (fun i (e : int array) -> setEdge e[0] e[1] e[2] i g) edges
+    Array.iteri (fun i (e : int array) -> setEdge e[0] e[1] e[2] i g) edges
     g
+
+let sortedEdges ( Graph (_, es, _) ) : int array =
+    let tupleEs = es |> Array.Parallel.mapi ( fun x (_, _, w) -> ( w , x ) ) 
+    tupleEs |> Array.sortInPlace
+    tupleEs |> Array.Parallel.map ( fun (_, x) -> x )
