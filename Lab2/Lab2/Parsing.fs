ed3c4de688458733b70139f9ced0f9489f98fb9e
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-05-05 16:38:00 +0200

    Beautified and commented code

--- Lab2/Lab2/Parsing.fs
+++ Lab2/Lab2/Parsing.fs
@@ -18,14 +18,12 @@ let getGraph (nodes: float array array) weightType: Graph =
     let N = nodes.Length
     let M = ( nodes.Length * (nodes.Length - 1) ) / 2
     let sizes = [| N; M |]
-    let edges : int array array = Array.zeroCreate M
-    
-    let mutable x = 0
-    for i = 0 to N - 1 do
-        for n = i + 1 to N - 1 do
-            edges[x] <- [| int nodes[i].[0]; int nodes[n].[0]; (w nodes[i].[1] nodes[i].[2] nodes[n].[1] nodes[n].[2] weightType) |]
-            x <- x + 1
-
+    let edges : int array array = 
+        [| 
+            for i = 0 to N - 1 do
+                for n = i + 1 to N - 1 do
+                    [| int nodes[i].[0]; int nodes[n].[0]; (w nodes[i].[1] nodes[i].[2] nodes[n].[1] nodes[n].[2] weightType) |]
+        |]
     (buildGraph edges sizes)
 
 let private (|Prefix|_|) (p:string) (s:string) =
