de5da3e81fd62365496de165c329bb304dce9112
Daniel Eduardo Contro <dani.contro2@gmail.com>
2022-05-06 15:10:46 +0200

    added times and generalized main code

--- Lab2/Lab2/Main.fs
+++ Lab2/Lab2/Main.fs
@@ -1,6 +1,7 @@
 ﻿module Lab2.Main
 
 open Lab1.Utils
+open Lab1.Graphs
 open TspGraph
 open Parsing
 open MetricTsp
@@ -14,17 +15,23 @@ let main _ =
     let files = getFiles (Directory.GetCurrentDirectory() +/ "tsp_dataset")
     let tspGraphs = Array.map buildGraph files
     
-    let algorithm alg iterations =
-        printfn "%9s\t%9s\t%9s" "Name" "Dimension" "Weight"
+    let algorithm alg iterations algName =
+        printfn $"%s{algName} algorithm:"
+        printfn "%9s\t%9s\t%9s\t%9s" "Name" "Dimension" "Weight" "Time"
         printfn "%s" (String.replicate 60 "-")
-        for tspGraph in tspGraphs do
-            let TspGraph (name, comment, dimension, G) as _ = tspGraph
-            alg G 
-            |> Seq.toList 
-            |> getTotalWeightFromTree G 
-            |> printfn "%9s\t%9i\t%9i" name dimension
-//        let runTimes = Array.map (fun g -> measureRunTime alg g iterations) tspGraphs
+        let names, dimensions, weights, runTimes =
+            tspGraphs |> Array.map (fun tspG ->
+                    let TspGraph (name, _, dimension, G) as _ = tspG
+                    let runTime = int (measureRunTime alg G iterations)
+                    (alg G) |> Seq.toList 
+                    |> getTotalWeightFromTree G
+                    |> (fun weight ->
+                        printfn $"%9s{name}\t%9i{dimension}\t%9i{weight}\t%9i{runTime}"
+                        (name, dimension, weight, runTime))
+                )
+                |> Array.fold (fun (N, D, W, T) (n, d, w, t) -> N @ [n], D @ [d], W @ [w], T @ [t]) (List.Empty, List.Empty, List.Empty, List.Empty)
+        printf "\n \n"
+        0
+            
 //    algorithm metricTsp 100
-    algorithm nearestNeighbourHeuristic 100
-    
-    0
+    algorithm nearestNeighbourHeuristic 100 "Nearest Neighbour"
