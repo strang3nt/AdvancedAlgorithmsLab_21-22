6e3d8d40bb95b3e77c200e48168797be1d5a88ff
Daniel Eduardo Contro <dani.contro2@gmail.com>
2022-05-06 10:56:05 +0200

    Fixed nearestNeighbour algorithm

--- Lab2/Lab2/Main.fs
+++ Lab2/Lab2/Main.fs
@@ -4,6 +4,7 @@ open Lab1.Utils
 open TspGraph
 open Parsing
 open MetricTsp
+open NearestNeighbourHeuristic
 open Utils
 
 open System.IO
@@ -13,12 +14,17 @@ let main _ =
     let files = getFiles (Directory.GetCurrentDirectory() +/ "tsp_dataset")
     let tspGraphs = Array.map buildGraph files
     
-    printfn "%9s\t%9s\t%9s" "Name" "Dimension" "Weight"
-    printfn "%s" (String.replicate 60 "-")
-    for tspGraph in tspGraphs do
-        let TspGraph (name, comment, dimension, G) as _ = tspGraph
-        metricTsp G 
-        |> Seq.toList 
-        |> getTotalWeightFromTree G 
-        |> printfn "%9s\t%9i\t%9i" name dimension
+    let algorithm alg iterations =
+        printfn "%9s\t%9s\t%9s" "Name" "Dimension" "Weight"
+        printfn "%s" (String.replicate 60 "-")
+        for tspGraph in tspGraphs do
+            let TspGraph (name, comment, dimension, G) as _ = tspGraph
+            alg G 
+            |> Seq.toList 
+            |> getTotalWeightFromTree G 
+            |> printfn "%9s\t%9i\t%9i" name dimension
+//        let runTimes = Array.map (fun g -> measureRunTime alg g iterations) tspGraphs
+//    algorithm metricTsp 100
+    algorithm nearestNeighbourHeuristic 100
+    
     0
--- Lab2/Lab2/NearestNeighbourHeuristic.fs
+++ Lab2/Lab2/NearestNeighbourHeuristic.fs
@@ -8,9 +8,10 @@ let initialisation (Graph (V,_,_)) =
     | [||] -> failwith "Invalid graph"
     | _ -> V[0] :: List.Empty
     
-let selection (Graph (_,E,adjList)) (partialCircuit: Node list) =
+let selection (Graph (V,E,adjList)) (partialCircuit: Node list) =
     let lastNode = partialCircuit |> List.last
-    adjList[lastNode] |> List.filter (fun x ->
+    let lastNodeIndex = lastNode - 1
+    adjList[lastNodeIndex] |> List.filter (fun x ->
         let u, v, _ = E[x]
         if u = lastNode then
             not (partialCircuit |> List.contains v)
@@ -22,9 +23,16 @@ let selection (Graph (_,E,adjList)) (partialCircuit: Node list) =
         w
     )
     |> List.head
+    |> (fun x ->
+        let i,j,_ = E[x]
+        if V[i] = lastNode then
+            V[j]
+        else
+            V[i]
+    )
 
 let insertion v partialCircuit =
     partialCircuit @ [v]
     
 
-let NearestNeighbourHeuristic G = ConstructiveHeuristic initialisation selection insertion G
+let nearestNeighbourHeuristic G = ConstructiveHeuristic initialisation selection insertion G
