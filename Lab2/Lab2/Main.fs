58bc930f1693d40fa6867b2106c508ff7fc66aa8
Daniel Eduardo Contro <dani.contro2@gmail.com>
2022-05-09 17:04:35 +0200

    Done(?)

--- Lab2/Lab2/Main.fs
+++ Lab2/Lab2/Main.fs
@@ -1,6 +1,7 @@
 ﻿module Lab2.Main
 
 open Lab1.Utils
+open Lab2.Utils
 open TspGraph
 open Parsing
 open MetricTsp
@@ -16,19 +17,25 @@ let main _ =
     
     let algorithm alg iterations algName =
         printfn $"%s{algName} algorithm:"
-        printfn "%9s\t%9s\t%9s\t%9s" "Name" "Dimension" "Weight" "Time"
+        printfn "%9s\t%9s\t%9s\t%12s" "Name" "Dimension" "Weight" "Time"
         printfn "%s" (String.replicate 60 "-")
         let names, dimensions, weights, runTimes =
             tspGraphs |> Array.map (fun tspG ->
                     let TspGraph (name, _, dimension, G) as _ = tspG
-                    let runTime = int (measureRunTime alg G iterations)
+                    let runTime = int64 (measureRunTime alg G iterations)
                     (alg G) |> Seq.toList 
                     |> getTotalWeightFromTree G
                     |> (fun weight ->
-                        printfn $"%9s{name}\t%9i{dimension}\t%9i{weight}\t%9i{runTime}"
+                        printfn $"%9s{name}\t%9i{dimension}\t%9i{weight}\t%12i{runTime}"
                         (name, dimension, weight, runTime))
                 )
                 |> Array.fold (fun (N, D, W, T) (n, d, w, t) -> N @ [n], D @ [d], W @ [w], T @ [t]) (List.Empty, List.Empty, List.Empty, List.Empty)
+                
+        saveToCSV   (Directory.GetCurrentDirectory() +/ "out" +/ algName)
+                    (names |> List.toArray)
+                    (dimensions |> List.toArray)
+                    (weights |> List.toArray)
+                    (runTimes |> List.toArray)
         printf "\n \n"
         0
             
--- Lab2/Lab2/NearestNeighbourHeuristic.fs
+++ Lab2/Lab2/NearestNeighbourHeuristic.fs
@@ -3,28 +3,43 @@ module Lab2.NearestNeighbourHeuristic
 open Lab1.Graphs
 open Lab2.ConstructiveHeuristic
 
+// Initialise the list of indexes of the partialCircuit with the index of the
+// first node of the given graph and create the map used to store information
+// about the already visited nodes in O(n)
 let initialisation (Graph (V,_,_)) =
     match V with
     | [||] -> failwith "Invalid graph"
-    | _ -> 0 :: List.Empty
+    | _ ->
+        let visitedMap = Map.ofArray ({ 0..V.Length-1} |> Seq.toArray |> Array.map (fun vIdx -> vIdx,false))
+        0 :: List.Empty, visitedMap |> Map.change 0 (fun _ -> Some true)
     
-let selection (Graph (_, E, adjList) as G) (partialCircuit: int list) =
-    let lastNode = partialCircuit |> List.head
-    adjList[lastNode]
+let selection (Graph (_, E, adjList) as G) (partialCircuitIndexes: int list) (visitedMap: Map<int, bool>) =
+    let lastNodeIdx = partialCircuitIndexes |> List.head
+//  Selects the adjacency list of the last node inserted in the partial circuit
+//  with complexity O(n)
+    adjList[lastNodeIdx]
+//  Filters the adjacency list of the last node by checking the visited map and
+// removing nodes already in the partial circuit with complexity O(n)
     |> List.filter (fun e ->
-        let u = opposite lastNode e G
-        not (partialCircuit |> List.contains u)
+        let uIdx = opposite lastNodeIdx e G
+        not (visitedMap[uIdx])
     )
-    |> List.minBy (fun e ->
-        let (_, _, w) = E[e]
+//  Takes the node associated to the edge with the minimum weight in O(n)
+    |> List.minBy (fun eIdx ->
+        let (_, _, w) = E[eIdx]
         w
     )
+//  O(1)
     |> (fun e ->
-        opposite lastNode e G
+        opposite lastNodeIdx e G
     )
 
-let insertion v partialCircuit =
-    v :: partialCircuit
+// Inserts the given node in the head of the partial circuit instead of
+// appending it as specified by the actual heuristic, in order to execute the
+// operation in O(1) and then updates the visited map setting as visited the
+// inserted node with complexity O(log n)
+let insertion vIdx partialCircuitIndexes visitedMap =
+    vIdx :: partialCircuitIndexes, visitedMap |> Map.change vIdx (fun _ -> Some true)
     
 
 let nearestNeighbourHeuristic G = ConstructiveHeuristic initialisation selection insertion G
