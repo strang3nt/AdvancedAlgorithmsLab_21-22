5f19acf9281bb1566361ad1bb75dcdd1ed09e755
Daniel Eduardo Contro <dani.contro2@gmail.com>
2022-05-11 11:09:28 +0200

    I'm fast af

--- Lab2/Lab2/NearestNeighbourHeuristic.fs
+++ Lab2/Lab2/NearestNeighbourHeuristic.fs
@@ -1,8 +1,6 @@
 module Lab2.NearestNeighbourHeuristic
 
-open System.Collections
 open System.Collections.Generic
-//open FSharpx.Collections
 open Lab1.Graphs
 open Lab2.ConstructiveHeuristic
 
@@ -13,28 +11,27 @@ let initialisation (Graph (V,_,_)) =
     match V with
     | [||] -> failwith "Invalid graph"
     | _ ->
-        let visited = [ for v in 0..V.Length-1 -> v, false] |> dict |> Dictionary
-        visited[0] <- true
-        [0], visited
+        let visitedMap = [ for v in 0..V.Length-1 -> v, false] |> dict |> Dictionary
+        visitedMap[0] <- true
+        [0], visitedMap
     
-let selection (Graph (_, E, adjList) as G) (partialCircuitIndexes: int list) (visited: Dictionary<int, bool>) =
+let selection (Graph (_, _, adjList) as G) (partialCircuitIndexes: int list) (visitedMap: Dictionary<int, bool>) =
+//  Gets the index of the last inserted node in the partial circuit
     let lastNodeIdx = partialCircuitIndexes |> List.head
 //  Selects the adjacency list of the last node inserted in the partial circuit
 //  with complexity O(1)
     adjList[lastNodeIdx]
-//  Filters the adjacency list of the last node by checking the visited map and
-// removing nodes already in the partial circuit with complexity O(n)
-    |> List.filter (fun e ->
+//  Returns the index of the first edge where the opposite node isn't in
+//  the partialCircuit, which means that, given the fact that the adjacency list is sorted
+//  in ascending order by weight, the node opposite node is the one with
+//  minimum weight that must be selected.
+//  The complexity of the method is O(n)
+    |> List.find (fun e ->
         let uIdx = opposite lastNodeIdx e G
-        not (visited[uIdx])
+        not (visitedMap[uIdx])
     )
-    |> List.head
-//  Takes the node associated to the edge with the minimum weight in O(n)
-//    |> List.minBy (fun eIdx ->
-//        let (_, _, w) = E[eIdx]
-//        w
-//    )
-//  O(1)
+//  Returns the index of the opposite node of the edge with minimum weight from
+//  the last inserted node in O(1)
     |> (fun e ->
         opposite lastNodeIdx e G
     )
@@ -43,9 +40,9 @@ let selection (Graph (_, E, adjList) as G) (partialCircuitIndexes: int list) (vi
 // appending it as specified by the actual heuristic, in order to execute the
 // operation in O(1) and then updates the visited map setting as visited the
 // inserted node with complexity O(log n)
-let insertion vIdx partialCircuitIndexes (visited: Dictionary<int, bool>) =
-    visited[vIdx] <- true
-    vIdx :: partialCircuitIndexes, visited
+let insertion vIdx partialCircuitIndexes (visitedMap: Dictionary<int, bool>) =
+    visitedMap[vIdx] <- true
+    vIdx :: partialCircuitIndexes, visitedMap
     
 
 let nearestNeighbourHeuristic G = ConstructiveHeuristic initialisation selection insertion G
