18d368f8abe6d2bbd03286fc1003ff509912119b
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-05-13 12:11:58 +0200

    Fixed closest insertion maybE

--- Lab2/Lab2/ClosestInsertionHeuristic.fs
+++ Lab2/Lab2/ClosestInsertionHeuristic.fs
@@ -1,6 +1,7 @@
 ﻿module Lab2.ClosestInsertionHeuristic
 
 open System.Collections.Generic
+open System
 open Lab1.Graphs
 open Lab2.ConstructiveHeuristic
 
@@ -21,25 +22,13 @@ let initialisation (Graph (V,E,A) as G) =
 // Select the vertex k not in the current circuit with minimum distance from it
 // The distance of a vertex is the minimum weight of an edge from k to any node
 // of the circuit
-let selection (Graph (V,E,adjList)) (partialCircuit: int list) (visitedMap: Dictionary<int, bool>) =
+let selection (Graph (V,E,adjList) as G) (partialCircuit: int list) (visitedMap: Dictionary<int, bool>) =
     seq { 0 .. V.Length - 1 }
-    |> Seq.filter (fun x -> not visitedMap[x])
+    |> Seq.filter (fun v -> not visitedMap[v])
     |> Seq.minBy (fun v -> 
-        let (_, _, w) = 
-            adjList[v]
-            |> List.minBy (fun e -> 
-                let x,y,w = E[e]
-                let edgeConnection = visitedMap[x] || visitedMap[y]
-                let w = if edgeConnection then w else System.Int32.MaxValue
-                w)
-            |> Array.get E
+        let e = List.find (fun e -> let u = opposite v e G in visitedMap[u]) adjList[v]
+        let (_, _, w) = E[e]
         w)
-
-// Type that indicates what a weight in the list is generated from
-// IJ and JK are for edges connecting a j node to either an i or a k node
-// Sum indicates that the weight is the sum of some other nodes's weights
-// NotRelated indicates weights of edges from j that are not needed for the result
-type EdgeType = IJ | JK | Sum | NotRelated
 
 // Find the i, j consecutive nodes in the circuit for which the triangular
 // inequality with the given node k (w_ik + w_jk - w_ij) has the minimum value,
@@ -51,43 +40,20 @@ let insertion k partialCircuit (Graph (_,E,adjList)) (visitedMap: Dictionary<int
         |> List.pairwise
 //  For each node j in the circuit, its predecessor i, and the target node k
 //  calculate (w_ik + w_jk - w_ij)
-        |> List.map (fun (i, j) ->
+        |> List.mapi (fun insertAt (i, j) ->
 //  Get the weight for the edge (i,k)
             let w_ik = getEdgeWeight adjList E i k
-            let _,w_jk_ij =
-                adjList[j]
-//  Find the (i,j) and (j,k) edges in the adjacency list and get their weights
-                    |> List.map (fun e -> 
-                        match E[e] with
-                        | x,y,w when x = i || y = i -> IJ, w
-                        | x,y,w when x = k || y = k -> JK, w
-                        | _ -> NotRelated, 0)
-//  Sum the weights according to formula
-                    |> List.reduce (fun e1 -> fun e2 ->
-                        let w = match e1 with
-                                | IJ, w_ij -> -w_ij
-                                | JK, w_jk -> w_jk
-                                | Sum, w
-                                | NotRelated, w -> w
-                        match e2 with
-                        | IJ, w_ij -> Sum, w - w_ij
-                        | JK, w_jk -> Sum, w + w_jk
-                        | Sum, w
-                        | NotRelated, w -> Sum, w)
+            let w_jk= getEdgeWeight adjList E j k 
+            let w_ij = getEdgeWeight adjList E i j
 //  Return the calculated value and the j node for which it was calculated
-            w_jk_ij + w_ik, j)
+            (w_ik + w_jk - w_ij), insertAt + 1)
 //  Get the vertex corresponding to the minimum value
         |> List.minBy (fun (w,_) -> w)
 
-//  Split the circuit at the vertex j and insert k before it
-    let circ1, circ2 = 
-        partialCircuit
-        |> List.findIndex (fun x -> x = j)
-        |> List.splitAt <| partialCircuit
 //  Set the node as visited
     visitedMap[k] <- true
 //  Return the expanded circuit and the updated map
-    circ1 @ [k] @ circ2, visitedMap
+    List.insertAt j k partialCircuit, visitedMap
 
 
 let closestInsertionHeuristic G = ConstructiveHeuristic initialisation selection insertion G
