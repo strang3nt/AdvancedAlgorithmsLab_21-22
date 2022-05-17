5646426925253cd1d24c19f82c193c6a60b989da
Dam-99 <damiano.mason99@gmail.com>
2022-05-17 11:39:05 +0200

    Created basic strucutre for Lab3

---
+++ Lab3/Lab3/Main.fs
@@ -0,0 +1,14 @@
+﻿module Lab3.Main
+
+open Lab1.Parsing
+open Lab1.Utils
+
+open System.IO
+
+
+let main _ =
+    let files = getFiles (Directory.GetCurrentDirectory() +/ "dataset")
+
+    let graphs = Array.Parallel.map buildGraph files
+    
+    printfn $"%i{graphs.Length} graphs"
