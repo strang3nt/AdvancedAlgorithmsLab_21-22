84d2706b78becd206ac23dd33782f30f29cb59f7
Alessandro Flori <alessandro.flori@studenti.unipd.it>
2022-05-17 13:08:15 +0200

    New MinCutGraph

--- Lab3/Lab3/Main.fs
+++ Lab3/Lab3/Main.fs
@@ -1,14 +1,16 @@
 ﻿module Lab3.Main
 
-open Lab1.Parsing
+open Lab3.Parsing
 open Lab1.Utils
 
 open System.IO
 
-
+[<EntryPoint>]
 let main _ =
     let files = getFiles (Directory.GetCurrentDirectory() +/ "dataset")
 
     let graphs = Array.Parallel.map buildGraph files
     
     printfn $"%i{graphs.Length} graphs"
+
+    0
