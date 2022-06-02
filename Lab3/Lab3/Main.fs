a9d0bb178faa01fe00c7d2ee2000ddb21b6404e1
Daniel Eduardo Contro <dani.contro2@gmail.com>
2022-06-02 08:55:34 +0200

    Gotta fix contracting I guess

--- Lab3/Lab3/Main.fs
+++ Lab3/Lab3/Main.fs
@@ -2,6 +2,7 @@
 
 open Parsing
 open KargerStein
+open StoerWagner
 open Graph
 open Lab1.Utils
 
@@ -11,6 +12,7 @@ open System.IO
 
 [<EntryPoint>]
 let main _ =
+//    let files = getFiles (Directory.GetCurrentDirectory() +/ "../../.." +/ "dataset")
     let files = getFiles (Directory.GetCurrentDirectory() +/ "dataset")
 
     let graphs = Array.Parallel.map buildGraph files
@@ -21,5 +23,9 @@ let main _ =
     |> Array.iteri (fun i (MinCutGraph (V, _, _, _, _ ) as g) -> 
         let k = int (floor (Math.Log2(V.Length))) 
         printfn $"Graph {i} has Min Cut weight of {Karger g k}")
+    
+    graphs |> Array.iteri (fun i g ->
+        let minCut = StoerWagner g
+        printfn $"Graph {i} has Min Cut weight of {CutWeight g minCut}")
 
     0
--- Lab3/Lab3/StoerWagner.fs
+++ Lab3/Lab3/StoerWagner.fs
@@ -4,8 +4,8 @@ open System.Collections.Generic
 open Lab1.Graphs
 open Lab3.Graph
 
-type Key = int * Weight
-
+[<Struct>]
+type Key = Key of (Weight * int)
 
 let CrossingEdges (MinCutGraph (V, E, _, _, _)) (S: Nodes) =
     E |> Array.filter (fun (uIdx,vIdx,_) ->
@@ -16,36 +16,52 @@ let CutWeight G (S: Nodes) =
     CrossingEdges G S |> Array.sumBy (fun (_,_,w) -> w) : int
     
     
-let merge (MinCutGraph (V, E, A, D, W) as G) =
-    G
-    
-let rec codiceBello (PQ: SortedSet<Key>) A s t =
-    let uIdx, _ = PQ.Max
-    match A[]
+let merge (MinCutGraph (V, E, A, D, W) as G) u v =
+    D[u] <- D[u] + D[v] - 2 * W[u, v]
+    D[v] <- 0
+    W[u, v] <- 0
+    W[v, u] <- 0
+    for w = 0 to V.Length - 1 do
+        if w <> u && w <> v then
+            W[u, w] <- W[u, w] + W[v, w]
+            W[w, u] <- W[w, u] + W[w, v]
+            W[v, w] <- 0
+            W[w, v] <- 0
+    MinCutGraph (V, E, A, D, W)
     
 let stMinCut (MinCutGraph (V, E, A, D, W) as G) =
+        
     let Q = SortedSet<Key>()
-    [|0..V.Length-1|] |> Array.iter (fun idx -> Q.Add(idx,0) |> ignore)
-    let mutable s = -1
-    let mutable t = -1
+    let keys = Array.create V.Length 0
+    
+    [|0..V.Length-1|] |> Array.iter (fun idx -> Q.Add (Key (keys[idx], idx)) |> ignore )
+    let mutable s: int option = None
+    let mutable t: int option = None
     
     while Q.Count > 0 do
-        let u, _ = Q.Max
+        let Key (_, uIdx) as k = Q.Max
+        Q.ExceptWith (seq { k })
         s <- t
-        t <- u
-        A[]
-        for uIdx, vIdx, w in A[uIdx] do
-            if Q.Contains (V[vIdx], 0) then
-                Q.Add 
-    V,V[0],V[1]
+        t <- Some uIdx
+        A[uIdx] |> List.iter (fun eIdx ->
+            let vIdx = opposite uIdx eIdx G
+//            let vIdx = V[vIdx]
+            let _, _, w = E[eIdx]
+            let key = Key (keys[vIdx], vIdx)
+            if Q.Contains key then
+                keys[vIdx] <- keys[vIdx] + w
+                Q.ExceptWith (seq { key })
+                Q.Add (Key (keys[vIdx], vIdx)) |> ignore
+            )
+    V |> Array.removeAt t.Value, s.Value, t.Value
     
-let rec GlobalMinCut (MinCutGraph (V, _, _, _, _) as G) =
+let rec StoerWagner (MinCutGraph (V, _, _, _, _) as G) =
     let CutWeight = CutWeight G
     if V.Length = 2 then
         [| V[0]; V[1] |] : Nodes
     else
-        let c1, _, _ = stMinCut G
-        let c2 = GlobalMinCut (merge G)
+        let c1, s, t = stMinCut G
+        let c2 = StoerWagner (merge G s t)
         if CutWeight c1 <= CutWeight c2 then
             c1
         else
