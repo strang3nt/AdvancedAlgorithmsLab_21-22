10ded88b30dded12c9e55e58ea7eaa3a75a71ef7
Daniel Eduardo Contro <dani.contro2@gmail.com>
2022-06-13 15:02:20 +0200

    fasdfasdf works

--- Lab3/Lab3/Main.fs
+++ Lab3/Lab3/Main.fs
@@ -19,15 +19,24 @@ let main _ =
     
     printfn $"%i{graphs.Length} graphs"
 
-//    graphs
-//    |> Array.iteri (fun i (MinCutGraph (V, _, _, _, _ ) as g) -> 
-//        let k = int (floor (Math.Log2(V.Length))) 
-//        printfn $"Graph {i} has Min Cut weight of {Karger g k}")
-//    
-//    graphs |> Array.iteri (fun i g ->
-//        let minCut = StoerWagner g
-//        printfn $"Graph {i} has Min Cut weight of {CutWeight g minCut}")
-    let MinCutGraph (V0, E0, A0, D0, W0) as g0 = graphs[0]
-    let MinCutGraph (V, E, A, D, W) as g1 = fixedMerge graphs[0] 5 8
+//    let minCuts = graphs |> Array.mapi (fun i (MinCutGraph (V, _, _, _, _ ) as g) -> 
+//        let k = int (floor (Math.Log2(V.Length)))
+//        let minCut = Karger g k
+//        printfn $"Graph {i} has Min Cut weight of {minCut}"
+//        minCut
+//        )
+    
+        
+//    let writer = new StreamWriter (Directory.GetCurrentDirectory() +/ "../../.." +/ "out" +/ "minCuts" + ".txt")
+//    minCuts |> Array.iter (fun minCut ->
+//        writer.WriteLine $"%i{minCut}"
+//    )
+//    writer.Close()
+
+    graphs |> Array.iteri (fun i g ->
+        let minCut = StoerWagner' g
+        printfn $"Graph {i} has Min Cut weight of {CutWeight g minCut}")
+//    let MinCutGraph (V0, E0, A0, D0, W0) as g0 = graphs[0]
+//    let MinCutGraph (V, E, A, D, W) as g1 = fixedMerge graphs[0] 5 8
 
     0
